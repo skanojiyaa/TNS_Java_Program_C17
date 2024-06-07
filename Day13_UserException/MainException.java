1b0c19ed469f662ef6113ea60755a508a9de52a8
skanojiyaa <150128344+skanojiyaa@users.noreply.github.com>
2024-06-07 11:22:21 +0530

    User Exception

---
+++ Day13_UserException/MainException.java
@@ -0,0 +1,16 @@
+package Day13_UserException;
+
+import java.util.*;
+
+public class MainException {
+	public static void main(String[] args){
+		//TODO Auto-generated method stub
+		ThrowException ob = new ThrowException();
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Marks : ");
+		int mark = sc.nextInt();
+		ob.setMarks(mark);
+		System.out.println("Marks : "+ob.getMarks());
+	}
+	
+}
