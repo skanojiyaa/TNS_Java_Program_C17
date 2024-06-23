35b078186c318f620d2c4b3e8239281286369fc0
skanojiyaa <150128344+skanojiyaa@users.noreply.github.com>
2024-06-23 15:17:51 +0530

    Add files via upload

---
+++ JDBC/src/Day20_Connectivity/DatabaseConnection1.java
@@ -0,0 +1,27 @@
+package Day20_Connectivity;
+
+import java.sql.*;
+import javax.sql.*;
+
+public class DatabaseConnection1 {
+
+	private static final String DB_USERNAME = "root";
+	private static final String DB_PASSWORD = "Sakshi@433";
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/tns";
+	static Connection cn;
+	public static void getConnection(){
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			cn = DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
+			System.out.println(cn);
+		}
+		catch(ClassNotFoundException e)   {
+			   // TODO Auto-generated catch block  
+			e.printStackTrace();
+		  } catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ JDBC/src/Day20_Connectivity/TestConnection.java
@@ -0,0 +1,10 @@
+package Day20_Connectivity;
+
+public class TestConnection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DatabaseConnection1.getConnection();
+	}
+
+}
